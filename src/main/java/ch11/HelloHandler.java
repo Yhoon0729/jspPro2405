2b603a5c4abd372af3342765d87b57346b794b71
KIC <yh0729@kakao.com>
2024-05-21 14:57:09 +0900

    1

---
+++ src/main/java/ch11/HelloHandler.java
@@ -0,0 +1,15 @@
+package ch11;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloHandler implements CommandHandler{
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		request.setAttribute("msg", "안녕하세용");
+		return "/ch11_model2/index.jsp";
+	}
+
+}
